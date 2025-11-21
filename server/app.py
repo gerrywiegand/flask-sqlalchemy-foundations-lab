a5f2c352d94764cccb628229aa137f47f068437d
Gerry <gerrywiegand@gmail.com>
2025-11-21 13:37:33 -0500

    finished

--- server/app.py
+++ server/app.py
@@ -1,27 +1,73 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response  # noqa: I001
 from flask_migrate import Migrate
 
-from models import db, Earthquake
+from models import db, Earthquake  # noqa: F401
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    body = {'message': 'Flask SQLAlchemy Lab 1'}
+    body = {"message": "Flask SQLAlchemy Lab 1"}
     return make_response(body, 200)
+
+
+@app.route("/earthquakes/<int:id>")
+def e_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    if earthquake:
+        body = {
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year,
+        }
+        status = 200
+    else:
+        body = {"message": f"Earthquake {id} not found."}
+        status = 404
+    return make_response(body, status)
+
+
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def e_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    for eq in earthquakes:
+        count = len(earthquakes)
+    if earthquakes:
+        body = {
+            "count": count,
+            "quakes": [
+                {
+                    "id": eq.id,
+                    "location": eq.location,
+                    "magnitude": eq.magnitude,
+                    "year": eq.year,
+                }
+                for eq in earthquakes
+            ],
+        }
+        status = 200
+    else:
+        body = {
+            "count": 0,
+            "quakes": [],
+        }
+        status = 200
+    return make_response(body, status)
+
 
 # Add views here
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -6,3 +6,14 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+
+
+class Earthquake(db.Model):
+    __tablename__ = "earthquakes"
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Earthquake (id={self.id}, magnitude={self.magnitude}, location='{self.location}', year={self.year})"
